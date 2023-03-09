d8f69e97ad3be0bea483ffc60bafa0fb36d86fc7
Dtorres1ro <46894535+Datorresp@users.noreply.github.com>
2023-03-08 20:35:45 -0500

    POM Login (#6)

--- cypress/e2e/saucedemo.login.cy.ts
+++ cypress/e2e/saucedemo.login.cy.ts
@@ -1,5 +1,6 @@
-describe("Verifying Login Process for SauceDemo", () => {
-});
+import { LoginPage } from "../page/index";
+
+const loginPage = new LoginPage();
 
 describe("Verifying Login Process for SauceDemo", () => {
   beforeEach(() => {
@@ -14,23 +15,20 @@ describe("Verifying Login Process for SauceDemo", () => {
       });
 
       it("Login should notify when a wrong username/password combination is used.", () => {
-        cy.get('[data-test="username"]').type("testUser");
-        cy.get('[data-test="password"]').type("testPass");
-        cy.get('[data-test="login-button"]').click();
+        loginPage.login("testUser", "testPass");
 
         // Assertion for error message
-        cy.get("[data-test='error']").should(
+        loginPage.getErrorMsg().should(
           "include.text",
           "Username and password do not match any user in this service")
+
       });
     });
 
   it("Login should work for existing user.", () => {
-    cy.get('[data-test="username"]').type("standard_user");
-    cy.get('[data-test="password"]').type("secret_sauce");
-    cy.get('[data-test="login-button"]').click();
+     loginPage.login("standard_user", "secret_sauce");
 
-    // Assertion that verifies that products list is displayed
-    cy.get(".title").should("have.text", "Products")
+      // Assertion that verifies that products list is displayed
+      cy.get(".title").should("have.text", "Products")
   });
 });
